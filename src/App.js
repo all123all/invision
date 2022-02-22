90f7e47e80965c2889e75c0cffac531a5e5d44cf
Henrique Marques <allyson2308h@gmail.com>
2022-02-22 05:35:39 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import SignInScreen from './SignIn/SignIn';
+
+function App() {
+  return (
+    <SignInScreen />
+  );
+}
+
+export default App;
---
+++ src/components/FormBreakLine.js
@@ -0,0 +1,12 @@
+import './FormBreakLine.css';
+
+function FormBreakLine() {
+    return(
+        <div className="line-container">
+            <div className="line"/>
+                <p className="line-text">Or</p>
+            <div className="line"/>
+        </div>
+    );
+}
+export default FormBreakLine;
